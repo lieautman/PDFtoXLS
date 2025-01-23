088dd888f7c7e782f749faec48b47e84a23f3759
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2025-01-23 22:57:27 +0200

    center items

--- src/PDFtoXLS/PDFtoXLSComponent.js
+++ src/PDFtoXLS/PDFtoXLSComponent.js
@@ -1,11 +1,24 @@
 function PDFtoXLSComponent({ extractText }) {
   return (
-    <input
-      type="file"
-      accept="application/pdf"
-      onChange={(event) => extractText(event)}
-      multiple
-    />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }}
+    >
+      <h1 style={{ padding: "50px" }}>
+        Upload one or more files with the specific format.
+      </h1>
+      <input
+        type="file"
+        accept="application/pdf"
+        onChange={(event) => extractText(event)}
+        multiple
+      />
+    </div>
   );
 }
 
