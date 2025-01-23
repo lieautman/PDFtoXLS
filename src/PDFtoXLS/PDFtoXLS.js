bd18c73cb51d120f1ff3f4785a5ecce6969735b0
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2025-01-23 22:32:36 +0200

    make func to take in more files

--- src/PDFtoXLS/PDFtoXLS.js
+++ src/PDFtoXLS/PDFtoXLS.js
@@ -3,50 +3,53 @@ import * as FileSaver from "file-saver";
 import * as XLSX from "xlsx";
 
 function extractText(event) {
-  const file = event.target.files[0];
-  console.log();
-  pdfToText(file)
-    .then((text) => {
-      //all text from PDF document
-      const match = text.match(/d\. Acţiuni(.*?)e\. Obligatiuni/);
-      if (match) {
-        //excel data
-        const excelData = [];
-        //am gasit text intre "d. Acţiuni" si "e. Obligatiuni"
-        const textBetweenWords = match[1].trim();
+  event.target.files.foreach((file) => {
+    pdfToText(file)
+      .then((text) => {
+        //all text from PDF document
+        const match = text.match(/d\. Acţiuni(.*?)e\. Obligatiuni/);
+        if (match) {
+          //excel data
+          const excelData = [];
+          //am gasit text intre "d. Acţiuni" si "e. Obligatiuni"
+          const textBetweenWords = match[1].trim();
 
-        //reduc spatiile multiple la ;
-        const textDividedBySemicolumn = textBetweenWords.replace(
-          /\s{2,}/g,
-          ";"
-        );
-        //salvez primele 2 valor
-        const list = textDividedBySemicolumn.trim().split(";");
-        excelData.push({
-          "Valoare actualizată lei": list.shift(),
-          "Pondere în activul total al fondului": list.shift()
-        });
-        //salvez restul valorilor
-        while (list.length > 0) {
+          //reduc spatiile multiple la ;
+          const textDividedBySemicolumn = textBetweenWords.replace(
+            /\s{2,}/g,
+            ";"
+          );
+          //salvez primele 2 valor
+          const list = textDividedBySemicolumn.trim().split(";");
           excelData.push({
-            "Denumire emitent": list.shift(),
-            "ISIN activ": list.shift(),
             "Valoare actualizată lei": list.shift(),
             "Pondere în activul total al fondului": list.shift()
           });
+          //salvez restul valorilor
+          while (list.length > 0) {
+            excelData.push({
+              "Denumire emitent": list.shift(),
+              "ISIN activ": list.shift(),
+              "Valoare actualizată lei": list.shift(),
+              "Pondere în activul total al fondului": list.shift()
+            });
+          }
+          //console.log(excelData);
+          //export excel
+          const ws = XLSX.utils.json_to_sheet(excelData);
+          const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+          const excelBuffer = XLSX.write(wb, {
+            bookType: "xlsx",
+            type: "array"
+          });
+          const data = new Blob([excelBuffer], { type: ".xlsx" });
+          FileSaver.saveAs(data, file.name.slice(0, -4) + ".xlsx");
+        } else {
+          console.log("No match found");
         }
-        //console.log(excelData);
-        //export excel
-        const ws = XLSX.utils.json_to_sheet(excelData);
-        const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-        const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-        const data = new Blob([excelBuffer], { type: ".xlsx" });
-        FileSaver.saveAs(data, file.name.slice(0, -4) + ".xlsx");
-      } else {
-        console.log("No match found");
-      }
-    })
-    .catch((error) => console.error("Text extraction failed", error));
+      })
+      .catch((error) => console.error("Text extraction failed", error));
+  });
 }
 
 function PDFtoXLS() {
